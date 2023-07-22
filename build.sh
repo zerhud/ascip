f74cd099b112a1b194e7c38a1d346cc65fa2eda4
zerhud <zerhud@gmail.com>
2023-07-22 15:10:38 +0300

    try to create ascip as structure

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-g++ -std=c++23 -fwhole-program -march=native ./test.cpp -o ascip_test 2>&1 | pee xsel less
-./ascip_test 2>&1 |less
+#g++ -std=c++23 -fwhole-program -march=native ./test.cpp -o ascip_test 2>&1 | pee xsel less
+#./ascip_test 2>&1 |less
+
+g++ -std=c++23 -fwhole-program -march=native \
+	./test2.cpp -o ascip2_test \
+	-fconcepts-diagnostics-depth=3 -fdiagnostics-color=always \
+	2>&1 | pee xsel less
+./ascip2_test 2>&1 |less
