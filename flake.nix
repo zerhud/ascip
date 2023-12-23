a98bcd94613574dae463f457b7148f13235c3f5e
zerhud <zerhud@gmail.com>
2023-12-23 03:45:15 +0300

    add clion to nativeBuildInputs

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       der = pkgs.gcc13Stdenv.mkDerivation {
         name = "ascip";
-        nativeBuildInputs = [pkgs.clang_15];
+        nativeBuildInputs = [pkgs.clang_15 pkgs.jetbrains.clion];
         installPhase = "mkdir -p \"$out/include\" && cp ascip.hpp -t \"$out/include\" && cp -rt \"$out/include\" ascip";
         buildPhase = "g++ -std=c++23 -fwhole-program -march=native ./test.cpp -o ascip_test && ./ascip_test";
         meta.description = "ascip is a clean cpp ascii parser, it doesn't requires any changes in using code, has no dependecies. single header file.";
