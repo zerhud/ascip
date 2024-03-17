0a753536ed6069270d6f23adc66e921e248081bd
zerhud <zerhud@gmail.com>
2024-03-17 10:23:54 +0300

    add test to right part of variant in rvariant

--- build/ascip.hpp
+++ build/ascip.hpp
@@ -1726,6 +1726,7 @@ constexpr static auto test_rvariant_val(auto r, auto&& maker, auto pr, auto&& sr
 		, cast<dbl_expr>(rv_lreq++ >> _char<'-'> >> rv_rreq(rmaker))
 		, int_
 		, cast<dbl_expr>(rv_lreq++ >> _char<'*'> >> rv_rreq(rmaker)) | cast<dbl_expr>(rv_lreq++ >> _char<'/'> >> rv_rreq(rmaker))
+		, cast<dbl_expr>(rv_lreq++ >> lit<"**"> >> rv_rreq(rmaker))
 		, fp
 		, quoted_string
 		, rv_result(_char<'('> >> rv_req >> _char<')'>)
@@ -1738,9 +1739,10 @@ constexpr static auto test_rvariant_val(auto r, auto&& maker, auto pr, auto&& sr
 	static_assert( !var_with_skip.b.template is_term<1>() );
 	static_assert(  var_with_skip.b.template is_term<2>() );
 	static_assert( !var_with_skip.b.template is_term<3>() );
-	static_assert(  var_with_skip.b.template is_term<4>() );
+	static_assert( !var_with_skip.b.template is_term<4>() );
 	static_assert(  var_with_skip.b.template is_term<5>() );
 	static_assert(  var_with_skip.b.template is_term<6>() );
+	static_assert(  var_with_skip.b.template is_term<7>() );
 
 	return r;
 }
@@ -1756,16 +1758,19 @@ constexpr static bool test_rvariant_dexpr() {
 	struct min_expr : dbl_expr {} ;
 	struct pls_expr : dbl_expr {} ;
 	struct div_expr : dbl_expr {} ;
+	struct pow_expr : dbl_expr {} ;
 	struct expr_rt : factory_t::template variant<
 		 pls_expr, min_expr,
 		 int,
 		 typename factory_t::template variant<mul_expr, div_expr>,
+		 pow_expr,
 		 double, decltype(mk_str())
 	       > {};
 	constexpr auto pls_ind = 0;
 	constexpr auto mul_ind = 3;
+	constexpr auto pow_ind = 4;
 	constexpr auto int_ind = 2;
-	constexpr auto fp_ind = 4;
+	constexpr auto fp_ind = 5;
 
 	constexpr auto maker = [](auto& r){ return typename factory_t::template unique_ptr<expr_rt>( new expr_rt{std::move(r)} ); };
 
@@ -1775,6 +1780,8 @@ constexpr static bool test_rvariant_dexpr() {
 	static_assert( get<int_ind>(*get<0>(get<mul_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 3, "1*5"))).left) == 1 );
 	static_assert( get<int_ind>(*get<0>(get<mul_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 3, "1*5"))).right) == 5 );
 	static_assert( get<int_ind>(*get<1>(get<mul_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 3, "1/5"))).right) == 5 );
+	static_assert( get<int_ind>(*get<pow_ind>(*get<0>(get<mul_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 6, "1*5**2"))).right).right) == 2 );
+	static_assert( get<int_ind>(*get<pow_ind>(*get<1>(get<mul_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 6, "1/5**2"))).right).right) == 2 );
 	static_assert( get<int_ind>(*get<pls_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 5, "1*5+7")).right) == 7 );
 	static_assert( get<int_ind>(*get<0>(get<mul_ind>(*get<pls_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 5, "1*5+7")).left)).left) == 1 );
 	static_assert( get<int_ind>(*get<0>(get<mul_ind>(*get<pls_ind>(test_rvariant_val<dbl_expr>(expr_rt{}, maker, 5, "1*5+7")).left)).right) == 5 );
