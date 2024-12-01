a378009e6d6ffa16a9f077da539fe103fa4f3298
zerhud <zerhud@gmail.com>
2024-12-01 23:48:01 +0300

    move context and value_resetter tests to own file for speed up compilation

--- ascip/details.hpp
+++ ascip/details.hpp
@@ -46,16 +46,6 @@ struct val_resetter {
 	}
 };
 
-constexpr void test_val_resetter() {
-	static_assert( []{
-		int val = 10;
-		{
-			val_resetter vr(&val, 20);
-			val /= (val==20);
-		}
-		return val; }() == 10 );
-}
-
 template<typename tag, typename val, typename next_ctx> struct context {
 	using tag_t = tag;
 	using next_t = next_ctx;
@@ -135,42 +125,6 @@ constexpr auto remove_from_ctx(auto&& ctx) {
 		if constexpr (last) return make_ctx<cur_t::tag_t>(ctx.v);
 		else return make_ctx<typename cur_t::tag_t>(ctx.v, ctx.next());
 	}
-}
-
-constexpr void test_context() {
-	struct v1_t{int v=1;}; struct v2_t{int v=2;}; struct v3_t{int v=4;}; struct v4_t{int v=8;};
-	struct t1_t{}; struct t2_t{}; struct t3_t{}; struct t4_t{};
-	static_assert( []{ constexpr v1_t v1;
-		constexpr auto ctx = make_ctx<t1_t>(v1);
-		return search_in_ctx<t1_t>(ctx).v;
-	}()==1, "can find in single context" );
-	static_assert( []{ v1_t v1; v2_t v2;
-		auto ctx2 = make_ctx<t2_t>(v2, make_ctx<t1_t>(v1));
-		return search_in_ctx<t1_t>(ctx2).v;
-	}()==1, "can find in 2 deep context" );
-	static_assert( []{ v1_t v1; v2_t v2;
-		auto ctx2 = make_ctx<t2_t>(v2, make_ctx<t1_t>(v1));
-		return search_in_ctx<t2_t>(ctx2).v;
-	}()==2, "can find in 2 deep context" );
-	static_assert( []{ v1_t v1; v3_t v3;
-		auto ctx3 = make_ctx<t3_t>(v3, make_ctx<t1_t>(v1));
-		return exists_in_ctx<t3_t>(ctx3) && exists_in_ctx<t1_t>(ctx3) && !exists_in_ctx<t2_t>(ctx3);
-	}()==true, "exists_in_ctx by tag only" );
-	static_assert( []{ v1_t v1; v2_t v2;
-		auto ctx2 = make_ctx<t1_t>(v2, make_ctx<t1_t>(v1));
-		return by_ind_from_ctx<0,t1_t>(ctx2).v;
-	}()==2, "can find by ind" );
-	static_assert( []{ v1_t v1; v2_t v2;
-		auto ctx2 = make_ctx<t1_t>(v2, make_ctx<t1_t>(v1));
-		return by_ind_from_ctx<1,t1_t>(ctx2).v;
-	}()==1, "can find by ind" );
-	static_assert(  exists_in_ctx<t1_t>(make_ctx<t1_t>(v1_t{})) );
-	static_assert( !exists_in_ctx<t2_t>(make_ctx<t1_t>(v1_t{})) );
-	(void)( static_cast<const decltype(ctx_not_found)&>(by_ind_from_ctx<0,t2_t>(make_ctx<t1_t>(v2_t{}, make_ctx<t1_t>(v1_t{})))) );
-	static_assert( []{
-		auto ctx1 = make_ctx<t1_t>(1, make_ctx<t1_t>(2, make_ctx<t2_t>(3)));
-		auto ctx2 = remove_from_ctx<t1_t>(ctx1);
-		return (search_in_ctx<t1_t>(ctx1)==1) + (2*(search_in_ctx<t1_t>(ctx2)==2)); }() == 3 );
 }
 
 #include "static_string.ipp"
