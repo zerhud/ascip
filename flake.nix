3c20167b81541698e1a18b5316db34f1d6a10e39
zerhud <zerhud@gmail.com>
2025-01-02 15:06:07 +0300

    update flake: use clang 19

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       der = pkgs.gcc14Stdenv.mkDerivation {
         name = "ascip";
         buildInputs = [ tref ];
-        nativeBuildInputs = [pkgs.clang_17];
+        nativeBuildInputs = [pkgs.clang_19];
         installPhase = "mkdir -p \"$out/include\" && cp ascip.hpp -t \"$out/include\" && cp -rt \"$out/include\" ascip";
         buildPhase = "g++ -std=c++23 -fwhole-program -march=native ./test.cpp -o ascip_test && ./ascip_test";
         meta.description = "ascip is a clean cpp ascii parser, it doesn't requires any changes in using code, has no dependecies. single header file.";
