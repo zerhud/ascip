d99d3bccad3ec4beedfc1ce73e9079ba75ae1872
zerhud <zerhud@gmail.com>
2025-01-15 15:58:37 +0300

    add a test for variant compilation time

--- tests/monomorphic.cpp
+++ tests/monomorphic.cpp
@@ -34,6 +34,30 @@ static_assert( [] {
 	return parse(test_rvariant_val(), +p::space, p::make_source("1 + 2 * (3+5)"), r);
 }() == 13 );
 
+struct value { char n{}; char v{}; std::vector<std::unique_ptr<value>> c; };
+struct container {
+    std::vector<std::unique_ptr<value>> c;
+};
+
+constexpr auto value_maker = [](auto& v){ v.emplace_back() = std::make_unique<value>(); return v.back().get(); };
+
+/* NOTE: this is a test for a long compilation bug in variant parser.
+ *       the test halts down the IDE, so it commented, but it should work and should to be compiled ~5 seconds
+static_assert( [] {
+	auto a = (p::char_<'a'>++ >> p::char_<'1'>++ >> -p::req<1>)(value_maker);
+	auto c = (p::char_<'c'>++ >> p::char_<'3'>++ >> -p::req<1>)(value_maker);
+	auto i = a;
+	auto parser = p::nop >> (a | c | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i
+		| i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i
+		| i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i
+		| i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i
+		| i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i | i
+		);
+	container r;
+	return parse(parser, +p::space, p::make_source("c3a1"), r.c);
+}() == 4, "there was a bug in variant and this parser was a long long time to compile" );
+*/
+
 int main(int,char**) {
   return 0;
 }
