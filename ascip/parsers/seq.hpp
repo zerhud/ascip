4171bb1c2407617a1a9954184184d2cc71139e53
zerhud <zerhud@gmail.com>
2025-01-20 01:18:04 +0300

    refactor seq: separate parse method

--- ascip/parsers/seq.hpp
+++ ascip/parsers/seq.hpp
@@ -98,30 +98,26 @@ template<typename... parsers> struct opt_seq_parser : base_parser<opt_seq_parser
 	constexpr parse_result parse_without_prep(auto&& ctx, auto src, auto& result) const {
 		return parse_and_store_shift<0,0>(std::forward<decltype(ctx)>(ctx), std::move(src), result);
 	}
-	constexpr parse_result parse(auto&& ctx, auto src, auto& result) const requires (!is_recursion_enabled) {
+	constexpr parse_result parse(auto&& ctx, auto src, auto& result) const {
 		if(!src) return -1;
 		auto nl_controller = make_new_line_count_resetter(ctx, result);
 		auto shift_store = 0;
 		auto cur_ctx = make_ctx<seq_shift_stack_tag, any_shift_tag>(&shift_store,
 		  make_ctx<seq_result_stack_tag>(&result, ctx ) ) ;
-		auto ret = parse_without_prep(cur_ctx, src, result);
+		auto ret = parse_rswitch(cur_ctx, std::move(src), result);
 		nl_controller.update(ret);
 		return ret;
 	}
-	constexpr parse_result parse(auto&& ctx, auto src, auto& result) const requires is_recursion_enabled {
-		if(!src) return -1;
+	constexpr parse_result parse_rswitch(auto&& ctx, auto src, auto& result) const requires (!is_recursion_enabled) {
+		return parse_without_prep(ctx, std::move(src), result);
+	}
+	constexpr parse_result parse_rswitch(auto&& ctx, auto src, auto& result) const requires is_recursion_enabled {
 		using mono_type = seq_details::monomorphic<decltype(src), std::decay_t<decltype(result)>>;
-		auto shift_store = 0;
 		const mono_type* mono_ptr;
-		auto cur_ctx = make_ctx<seq_shift_stack_tag, any_shift_tag>(&shift_store,
-		  make_ctx<seq_result_stack_tag>(&result,
-		  	make_ctx<seq_stack_tag>(&mono_ptr, ctx) ) ) ;
+		auto cur_ctx = make_ctx<seq_stack_tag>(&mono_ptr, ctx) ;
 		auto mono = seq_details::mk_mono(this, cur_ctx, src, result);
 		mono_ptr = &mono;
-		auto nl_controller = make_new_line_count_resetter(ctx, result);
-		auto ret = mono.parse_mono(src, result);
-		nl_controller.update(ret);
-		return ret;
+		return mono.parse_mono(std::move(src), result);
 	}
 };
 template<typename... p> opt_seq_parser(p...) -> opt_seq_parser<std::decay_t<p>...>;
